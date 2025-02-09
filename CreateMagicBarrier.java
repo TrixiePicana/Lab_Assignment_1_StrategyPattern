8e7e2420f202580ea7304a62489de6d86aa08928
TrixiePicana <152823734+TrixiePicana@users.noreply.github.com>
2025-02-09 22:34:20 +0800

    Delete CreateMagicBarrier.java

--- CreateMagicBarrier.java
+++
@@ -1,8 +0,0 @@
-public class CreateMagicBarrier implements DefenseStrategy
-{
-    @Override
-    public void defend()
-    {
-        System.out.println("Creating a magic barrier for defense!");
-    }
-}
